07fa6c989be7148786f4f20beb9519606d99ca15
Isak Jacobsson <isak2jacobsson@gmail.com>
2025-12-10 21:38:37 +0100

    2025: day9 part2 testing

--- 2025/day9/main.py
+++ 2025/day9/main.py
@@ -23,104 +23,101 @@ print(largest_area)
 
 # Part 2
 
-all_points = {(int(x), int(y)) for row in rows for x, y in [row.split(",")]}
+start = time.perf_counter()
+largest_area = 0
 
-parimeter = set()
+
+def is_intersecting(v1, v2, dir):
+    if v1[0][0] == v1[1][0]:
+        # Vector 1 is vertical
+        if v1[0][1] <= v2[0][1] <= v1[1][1] or v1[1][1] <= v2[0][1] <= v1[0][1]:
+            if v2[0][0] <= v1[0][0] <= v2[1][0] or v2[1][0] <= v1[0][0] <= v2[0][0]:
+                if v1[0] == v2[0] or v1[0] == v2[1] or v1[1] == v2[0] or v1[1] == v2[1]:
+                    return False
+                if v2[0][0] == v1[0][0]:
+                    return (v2[0][0] - v2[1][0]) * dir < 0
+                if v2[1][0] == v1[0][0]:
+                    return (v2[1][0] - v2[0][0]) * dir < 0
+                return True
+    else:
+        # Vector 1 is horizontal
+        if v1[0][0] <= v2[0][0] <= v1[1][0] or v1[1][0] <= v2[0][0] <= v1[0][0]:
+            if v2[0][1] <= v1[0][1] <= v2[1][1] or v2[1][1] <= v1[0][1] <= v2[0][1]:
+                if v1[0] == v2[0] or v1[0] == v2[1] or v1[1] == v2[0] or v1[1] == v2[1]:
+                    return False
+                if v2[0][1] == v1[0][1]:
+                    return (v2[0][1] - v2[1][1]) * dir < 0
+                if v2[1][1] == v1[0][1]:
+                    return (v2[1][1] - v2[0][1]) * dir < 0
+                return True
+    return False
+
+
+hor_vecs = []
+ver_vecs = []
+
 for i in range(nbr_rows):
     x1, y1 = map(int, rows[i].split(","))
     x2, y2 = map(int, rows[(i + 1) % nbr_rows].split(","))
     if x1 == x2:
-        if y1 < y2:
-            for y in range(y1, y2 + 1):
-                parimeter.add((x1, y))
-        else:
-            for y in range(y2, y1 + 1):
-                parimeter.add((x1, y))
-
-    elif y1 == y2:
-        if x1 < x2:
-            for x in range(x1, x2 + 1):
-                parimeter.add((x, y1))
-        else:
-            for x in range(x2, x1 + 1):
-                parimeter.add((x, y1))
-
-largest_area = 0
-
-
-# def is_inside_parimeter(x1, y1, x2, y2):
-#     start_x = 0
-#     end_x = 0
-#     start_y = 0
-#     end_y = 0
-#     if x1 > x2:
-#         start_x = x2 + 1
-#         end_x = x1 - 1
-#     elif x1 < x2:
-#         start_x = x1 + 1
-#         end_x = x2 - 1
-#     else:
-#         start_x = end_x = x1
-#
-#     if y1 > y2:
-#         start_y = y2 + 1
-#         end_y = y1 - 1
-#     elif y1 < y2:
-#         start_y = y1 + 1
-#         end_y = y2 - 1
-#     else:
-#         start_y = end_y = y1
-#
-#     for x in range(start_x, end_x + 1):
-#         for y in range(start_y, end_y + 1):
-#             if (x, y) in parimeter:
-#                 return False
-#
-#     return True
-
-
-def check_if_points_not_inside_area(x1, y1, x2, y2):
-    start_x = 0
-    end_x = 0
-    start_y = 0
-    end_y = 0
-    if x1 > x2:
-        start_x = x2 + 1
-        end_x = x1 - 1
-    elif x1 < x2:
-        start_x = x1 + 1
-        end_x = x2 - 1
+        # Vertical
+        ver_vecs.append(((x1, y1), (x2, y2)))
     else:
-        start_x = end_x = x1
-
-    if y1 > y2:
-        start_y = y2 + 1
-        end_y = y1 - 1
-    elif y1 < y2:
-        start_y = y1 + 1
-        end_y = y2 - 1
-    else:
-        start_y = end_y = y1
-
-    for x in range(start_x, end_x + 1):
-        for y in range(start_y, end_y + 1):
-            if (x, y) in all_points:
-                return False
-
-    return True
+        # Horizontal
+        hor_vecs.append(((x1, y1), (x2, y2)))
 
 
-start = time.perf_counter()
+def check_intersections(ver, hor):
+    for side, dir in ver:
+        for side2 in hor_vecs:
+            if is_intersecting(side, side2, dir):
+                return True
+    for side, dir in hor:
+        for side2 in ver_vecs:
+            if is_intersecting(side, side2, dir):
+                return True
+    return False
 
-for i in range(nbr_rows):
+
+amount_ok = 0
+for i in tqdm(range(nbr_rows)):
     x1, y1 = map(int, rows[i].split(","))
-    for j in tqdm(range(i, nbr_rows)):
+    for j in range(i, nbr_rows):
         x2, y2 = map(int, rows[j].split(","))
 
-        if check_if_points_not_inside_area(x1, y1, x2, y2):
-            area = (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
-            largest_area = max(area, largest_area)
+        area = (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+        #if area < 436525686:  # We know this from a previous guess
+            #continue
+
+        sides_hor = []
+        sides_ver = []
+        sides_hor.append(((x2, y1), (x1, y1)))
+        sides_hor.append(((x2, y2), (x1, y2)))
+
+        if y1 > y2:
+            sides_hor[0] = (sides_hor[0], -1)
+            sides_hor[1] = (sides_hor[1], 1)
+        else:
+            sides_hor[0] = (sides_hor[0], 1)
+            sides_hor[1] = (sides_hor[1], -1)
+
+        sides_ver.append(((x2, y2), (x2, y1)))
+        sides_ver.append(((x1, y2), (x1, y1)))
+
+        if x1 > x2:
+            sides_ver[0] = (sides_ver[0], 1)
+            sides_ver[1] = (sides_ver[1], -1)
+        else:
+            sides_ver[0] = (sides_ver[0], -1)
+            sides_ver[1] = (sides_ver[1], 1)
+
+        if check_intersections(sides_ver, sides_hor):
+            continue
+
+        amount_ok += 1
+        largest_area = max(area, largest_area)
 
 end = time.perf_counter()
 print(f"Elapsed: {end - start:.6f} seconds")
+print(amount_ok)
 print(largest_area)
