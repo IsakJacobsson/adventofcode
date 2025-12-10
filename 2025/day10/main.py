0a7c70d9cc15e957fbffffd32302fed62afdb286
Isak Jacobsson <isak2jacobsson@gmail.com>
2025-12-10 17:01:57 +0100

    2025: day10 part1 optimization

--- 2025/day10/main.py
+++ 2025/day10/main.py
@@ -1,18 +1,20 @@
+import time
+
 with open("input.txt", "r") as f:
     s = f.read().strip()
 
 
 def find_min(goal, switches):
     current = [False] * len(goal)
-    found = find_min_helper(goal, current, switches, 0) - 1
+    found = find_min_helper(goal, current, switches, 0)
     if found > 5:
-        found = find_min_helper(goal, current, switches, 0, 7) - 1
+        found = find_min_helper(goal, current, switches, 0, 7)
     return found
 
 
 def find_min_helper(goal, current, switches, depth, max_depth=5):
     if goal == current:
-        return 1
+        return depth
     if depth > max_depth:
         return 1000
 
@@ -21,19 +23,17 @@ def find_min_helper(goal, current, switches, depth, max_depth=5):
         copy_current = current[:]
         for s in switches[i]:
             copy_current[s] = not copy_current[s]
-        copy_switches = switches[:]
-        copy_switches.pop(i)
+        copy_switches = switches[i + 1 :]  # No need to check different orders of clicks
         best = min(
             best,
-            find_min_helper(goal, copy_current, copy_switches, depth + 1, max_depth)
-            + 1,
+            find_min_helper(goal, copy_current, copy_switches, depth + 1, max_depth),
         )
     return best
 
 
+start = time.perf_counter()
 res = 0
 for row in s.split("\n"):
-    print(row)
     goal, *switches, other = row.split()
     goal = [False if l == "." else True for l in goal[1:-1]]
     switches = [[int(s) for s in switch[1:-1].split(",")] for switch in switches]
@@ -41,7 +41,8 @@ for row in s.split("\n"):
     # print(switches)
 
     nbr = find_min(goal, switches)
-    print(nbr)
     res += nbr
 
+end = time.perf_counter()
+print(f"Elapsed: {end - start:.6f} seconds")
 print(res)
